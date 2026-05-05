96dd7a8f47d0c557a5236e10708f4a153b638bcb
Kaelyn <you@example.com>
2026-05-05 13:49:48 -0400

    guinea_pig.py

---
+++ guinea_pig.py
@@ -0,0 +1,31 @@
+class GuineaPig: 
+    def __init__(self, name: str, color: str, weight: float): 
+        self.name = name 
+        self.color = color
+        self.weight = weight
+
+    def squeak(self) -> str: 
+        return f'{self.name} the {self.color} guinea pig squeaks!'
+    
+
+if __name__ == '__main__':
+    print('welcome to kaelyn\'s pet shop!')
+    
+    potato = GuineaPig('Potato', 'calico', 1.2)
+
+    print(potato.squeak())
+
+    class GuineaPig: 
+        def __init__(self, name: str, color: str, weight: float):
+            self.name = name 
+            self.color = color
+            self.weight = weight
+
+        def squeak(self) -> str: 
+            return f'{self.name} the {self.color} guinea pig squeaks!'
+
+if __name__== '__main__': 
+    bun = GuineaPig('Bun', 'black', 1.7) 
+
+    print(bun.squeak())
+
--- shop.py
+++ shop.py
@@ -1,4 +1,8 @@
-
+from guinea_pig import GuineaPig
 
 if __name__ == '__main__':
     print('welcome to kaelyn\'s pet shop!')
+    
+    potato = GuineaPig('Potato', 'calico', 1.2)
+
+    print(potato.squeak())
